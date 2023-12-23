9f1c45f29ed6fa4b2e57053f5e92506d4631340f
saberhark <34380276+saberhark@users.noreply.github.com>
2023-12-23 16:29:57 +0900

    d

--- mapleLand/convert_data_yolov5.py
+++ mapleLand/convert_data_yolov5.py
@@ -1,12 +1,14 @@
 import os
+import shutil
 import xml.etree.ElementTree as ET
 
 # 주어진 클래스 이름에 대한 ID 매핑
-classes = {'me': 0, 'name': 1, 'monster': 2, 'ladder': 3}
+classes = {'me': 0, 'name': 1, 'horn_mush': 2, 'zombie_mush': 3, 'ladder': 4}
 class_ids = {name: idx for idx, name in enumerate(classes)}
 
 
 def convert_annotation(xml_file, output_txt_file):
+    # XML 파일을 파싱
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
@@ -26,6 +28,7 @@ def convert_annotation(xml_file, output_txt_file):
 
 
 def convert_to_yolo_format(box, img_width, img_height):
+    # 박스 정보를 YOLO 형식으로 변환
     x_min, y_min, x_max, y_max = box
     x_center = (x_min + x_max) / 2 / img_width
     y_center = (y_min + y_max) / 2 / img_height
@@ -34,11 +37,22 @@ def convert_to_yolo_format(box, img_width, img_height):
     return x_center, y_center, width, height
 
 
-# 'annotations' 폴더 내의 모든 XML 파일을 YOLO 형식으로 변환합니다.
-# 해당 디렉토리와 파일들은 실제 환경에 맞게 조정되어야 합니다.
-for xml_file in os.listdir(r'./frame'):
-    if xml_file.endswith('.xml'):
-        output_txt_file = os.path.join(r'./frame', os.path.splitext(xml_file)[0] + '.txt')
-        convert_annotation(os.path.join(r'./frame', xml_file), output_txt_file)
+# 'a' 폴더 생성 (이미 존재하지 않는 경우)
+output_folder = r'./train'
+os.makedirs(output_folder, exist_ok=True)
 
-print("Conversion completed.")
+
+# 'zombie' 폴더 내의 모든 XML 파일을 처리
+for xml_file in os.listdir(r'./zombie'):
+    if xml_file.endswith('.xml'):
+        base_filename = os.path.splitext(xml_file)[0]
+        output_txt_file = os.path.join(output_folder, base_filename + '.txt')
+        convert_annotation(os.path.join(r'./zombie', xml_file), output_txt_file)
+
+        # 원본 이미지 파일 찾기 및 복사
+        image_file = os.path.join(r'./zombie', base_filename + '.jpg')
+        if os.path.exists(image_file):
+            shutil.copy(image_file, output_folder)
+
+
+print("Conversion and copying completed.")
--- mapleLand/pretreatment.py
+++ mapleLand/pretreatment.py
@@ -1,58 +1,32 @@
 import os
-import shutil
-import cv2
-from sklearn.model_selection import train_test_split
-from PIL import Image, ImageOps
 import random
-import os
 
-# 라벨별 데이터 갯수 파악
-def data_count():
+import cv2
+from PIL import Image, ImageOps
+from sklearn.model_selection import train_test_split
+
+
+def data_count(path):
     label_dict = {}
-    # 'dataset/labels/train' 폴더의 모든 파일을 순회
-    for filename in os.listdir('train'):
+    for filename in os.listdir(path):
         if filename.endswith('.txt'):
-            with open(f'train/{filename}', 'r') as file:
+            with open(os.path.join(path, filename), 'r') as file:
                 labels = file.readlines()
                 for label_line in labels:
-                    # 라인에서 첫 번째 공백 전까지의 문자열을 추출하여 라벨로 사용
                     label = int(label_line.split()[0])
-                    # 해당 라벨의 발견 횟수를 label_dict에 저장하고, 이미 존재하는 경우 값을 1 증가
                     label_dict[label] = label_dict.get(label, 0) + 1
+    return label_dict
 
-    # label_dict를 출력
-    print("Label Dictionary:")
-    for label, count in label_dict.items():
-        print(f'{label}: {count}')
+def separate_train_val(image_dir, test_size=0.2):
+    image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith(('.jpg', '.png'))]
+    label_paths = [p.replace('.jpg', '.txt').replace('.png', '.txt') for p in image_paths]
 
-def separate_train_val():
-    # 이미지 파일 경로와 라벨 파일 경로를 저장할 리스트
-    image_paths = []
-    label_paths = []
-
-    # 이미지 파일과 라벨을 로드하는 코드
-    for filename in os.listdir('train'):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join('train', filename)
-            label_path = os.path.join('train', os.path.splitext(filename)[0] + '.txt')
-
-            image_paths.append(image_path)
-            label_paths.append(label_path)
-
-    # 훈련 데이터와 검증 데이터로 분할 (8:2 비율)
     train_image_paths, val_image_paths, train_label_paths, val_label_paths = train_test_split(
-        image_paths, label_paths, test_size=0.2
+        image_paths, label_paths, test_size=test_size
     )
     return train_image_paths, val_image_paths, train_label_paths, val_label_paths
 
-def copy_files(file_paths, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
 
-    for path in file_paths:
-        shutil.copy(path, output_folder)
-
-# 이미지를 그레이스케일로 변환하고 저장하는 함수
 def convert_to_grayscale_and_save(image_paths, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -62,52 +36,51 @@ def convert_to_grayscale_and_save(image_paths, output_folder):
         output_path = os.path.join(output_folder, os.path.basename(path))
         cv2.imwrite(output_path, img)
 
-# 훈련 데이터와 검증 데이터를 분할하고 복사
-train_image_paths, val_image_paths, train_label_paths, val_label_paths = separate_train_val()
-convert_to_grayscale_and_save(train_image_paths, r'./dataset/train_grayscale')
-convert_to_grayscale_and_save(val_image_paths, r'./dataset/val_grayscale')
-copy_files(train_label_paths, r'./dataset/train_labels')
-copy_files(val_label_paths, r'./dataset/val_labels')
-
-
 
 def augment_image(image_path, output_path, augment_count=5):
-    """ 이미지를 읽고, 증강 후 저장 """
-    image = Image.open(image_path).convert('L')  # 그레이스케일로 변환
+    image = Image.open(image_path).convert('L')
     for i in range(augment_count):
         augmented_image = image.copy()
 
-        # 좌우 반전
         if random.random() > 0.5:
             augmented_image = ImageOps.mirror(augmented_image)
 
-        # 절단 및 가림
         augmented_image = crop_and_cover(augmented_image)
-
         augmented_image.save(os.path.join(output_path, f"{i}_{os.path.basename(image_path)}"))
 
+
 def crop_and_cover(image):
-    """ 이미지의 일부를 절단하거나 가림 """
     w, h = image.size
     crop_x = random.randint(0, w // 4)
     crop_y = random.randint(0, h // 4)
 
-    # 이미지 절단
     if random.random() > 0.5:
         image = image.crop((crop_x, crop_y, w - crop_x, h - crop_y))
-
-    # 이미지 가림
     else:
         cover = Image.new('L', (crop_x, crop_y), 'black')
         image.paste(cover, (random.randint(0, w - crop_x), random.randint(0, h - crop_y)))
 
     return image
 
-# 증강할 이미지의 경로
-image_path = 'path/to/your/image.jpg'
 
-# 증강된 이미지를 저장할 경로
-output_path = 'path/to/augmented/images'
+if __name__ == '__main__':
+    # 데이터셋 디렉터리 설정
+    train_dir = 'path/to/train/dataset'
 
-# 이미지 증강 실행
-augment_image(image_path, output_path, augment_count=5)
+    # 데이터 카운트
+    label_counts = data_count(train_dir)
+    print("Label counts:", label_counts)
+
+    # 훈련 및 검증 데이터 분할
+    train_images, val_images, train_labels, val_labels = separate_train_val(train_dir)
+
+    # 이미지를 그레이스케일로 변환하고 저장
+    convert_to_grayscale_and_save(train_images, './dataset/train_grayscale')
+    convert_to_grayscale_and_save(val_images, './dataset/val_grayscale')
+
+    # 증강 이미지 저장 경로 설정
+    augmented_image_path = 'path/to/augmented/images'
+
+    # 이미지 증강 실행
+    for image_path in train_images:
+        augment_image(image_path, augmented_image_path)
